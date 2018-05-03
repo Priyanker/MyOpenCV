670dcd181662a5b4ad87d9dacaf4a97416703258
Priyanker <priyankerrao@gmail.com>
2018-05-03 14:26:18 +0530

    First Commit
    Hello program

---
+++ OpenCVLearning/src/learning/HelloCV.java
@@ -0,0 +1,15 @@
+package learning;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+
+public class HelloCV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
+	}
+
+}
